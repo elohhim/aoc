038d950f04c78f173fbdf888577620954ca43cf1
Jan <elohhim@gmail.com>
2021-02-27 01:28:38 +0100

    AoC 2015, Day 8, Python

---
+++ python/aoc/2015/08.py
@@ -0,0 +1,29 @@
+
+import ast
+from typing import List
+
+
+def parse_data(data: str) -> List[str]:
+    return [l.strip() for l in data.split('\n')]
+
+
+def sum_len(ls: List[str]) -> int:
+    return sum(map(len, ls))
+
+
+def solve_1(data: str) -> int:
+    lines = parse_data(data)
+    eval_lines = [ast.literal_eval(l) for l in lines]
+    return sum_len(lines) - sum_len(eval_lines)
+
+
+def solve_2(data: str) -> int:
+    lines = parse_data(data)
+
+    def escape(l: str) -> str:
+        r = l.replace("\\", "\\\\").replace('"', '\\"')
+        return f'"{r}"'
+
+    escaped_lines = [escape(l) for l in lines]
+
+    return sum_len(escaped_lines) - sum_len(lines)
---
+++ scripts/stub_py_file.ps1
@@ -0,0 +1,26 @@
+param ($year, $day)
+
+$padded_day = '{0:d2}' -f [int]$day
+$dir_path = "$(Get-Location)\..\python\aoc\$($year)"
+$files = "$($padded_day).py"
+
+Write-Host "Creating stub .py files for AoC Year $($year) Day $($padded_day)"
+New-Item -ItemType Directory -Force -Path $dir_path
+
+foreach ($f in $files) {
+    $file_path = "$($dir_path)\$($f)"
+    if (Test-Path $file_path) {
+        Write-Host "$($file_path) already exist"
+    }
+    else {
+        New-Item -ItemType File -Path $dir_path -Name $f
+$content = @"
+def solve_1(data: str) -> int:
+    pass
+
+def solve_2(data: str) -> int:
+    pass
+"@ 
+        [IO.File]::WriteAllLines($file_path, $content)
+    }
+}
