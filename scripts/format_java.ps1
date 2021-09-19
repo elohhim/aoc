8bd091118937fa732f65d738eb90f698cfd7f010
Jan <elohhim@gmail.com>
2021-09-19 22:04:55 +0200

    VSCode Java formatting

---
+++ scripts/format_java.ps1
@@ -0,0 +1,14 @@
+param ($file)
+
+$temp_dir = ".temp"
+$gjf_path = ".\$($temp_dir)\google-java-format-1.9-all-deps.jar"
+$gjf_release_url = "https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar"
+
+if (-Not (Test-Path $gjf_path)) {
+    New-Item -ItemType Directory -Path $temp_dir
+    Write-Host "Downloading google-java-formatter"
+    Invoke-WebRequest -Uri $gjf_release_url -OutFile $gjf_path
+}
+
+Write-Host $file
+java -jar $gjf_path --replace $file
