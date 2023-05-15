1263e7a4e00fadf96f93607da151f802525a3fca
Jan <elohhim@gmail.com>
2023-05-15 18:56:45 +0200

    AoC Viewer - Bump builders Angular devkit and TS

--- viewer/builders/src/generate-aoc-metadata.spec.ts
+++ viewer/builders/src/generate-aoc-metadata.spec.ts
@@ -4,8 +4,8 @@ describe('eventKeys', () => {
   test('should generate event keys for 2015 and 2016', () => {
     const result = [...eventKeys({ first: 2015, last: 2016 })];
 
-    expect(result.length).toBe(40);
+    expect(result.length).toBe(50);
     expect(result[0]).toEqual({ event: 2015, day: 1 });
-    expect(result[39]).toEqual({ event: 2016, day: 20 });
+    expect(result[49]).toEqual({ event: 2016, day: 25 });
   });
 });
