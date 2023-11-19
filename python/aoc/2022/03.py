db82ba7603a9c1da3171bfe92f4ad59d84f099b2
Jan <elohhim@gmail.com>
2023-11-19 23:24:04 +0100

    AoC 2022, Day 03, Python

---
+++ python/aoc/2022/03.py
@@ -0,0 +1,52 @@
+from functools import reduce
+from typing import List, Set, Tuple
+
+
+Item = str
+Compartment = str
+Rucksack = Tuple[Compartment, Compartment]
+Group = Tuple[Rucksack, Rucksack, Rucksack]
+
+
+def parse_row(row: str) -> Rucksack:
+    midpoint = len(row) // 2
+    return (row[:midpoint], row[midpoint:])
+
+
+def parse_data(data: str) -> List[Rucksack]:
+    rows = data.split("\n")
+    return [parse_row(row) for row in rows]
+
+
+def get_item_priority(item: Item) -> int:
+    return ord(item) - (96 if item.islower() else 38)
+
+
+def get_rucksack_priority(rucksack: Rucksack) -> int:
+    dupe = (set(rucksack[0]) & set(rucksack[1])).pop()
+    return get_item_priority(dupe)
+
+
+def solve_1(data: str) -> int:
+    rucksacks = parse_data(data)
+    return sum(get_rucksack_priority(r) for r in rucksacks)
+
+
+def group_rucksacks(rucksacks: List[Rucksack]) -> List[Group]:
+    return list(zip(rucksacks[:-2:3], rucksacks[1:-1:3], rucksacks[2::3]))
+
+
+def rucksack2set(rucksack: Rucksack) -> Set[Item]:
+    return set(rucksack[0] + rucksack[1])
+
+
+def get_group_priority(group: Group) -> int:
+    r1, r2, r3 = group
+    badge = (rucksack2set(r1) & rucksack2set(r2) & rucksack2set(r3)).pop()
+    return get_item_priority(badge)
+
+
+def solve_2(data: str) -> int:
+    rucksacks = parse_data(data)
+    groups = group_rucksacks(rucksacks)
+    return sum(get_group_priority(g) for g in groups)
