27c4e169e0f0557057ab408910e05c6c9bd71e59
Jan <elohhim@gmail.com>
2023-11-24 01:08:27 +0100

    AoC, Java - chore
    - vscode launch fixe
    - java formatting script fixes
    - gradle bump

--- scripts/format_java.ps1
+++ scripts/format_java.ps1
@@ -1,8 +1,8 @@
 param ($file)
 
 $temp_dir = ".temp"
-$gjf_path = ".\$($temp_dir)\google-java-format-1.9-all-deps.jar"
-$gjf_release_url = "https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar"
+$gjf_path = ".\$($temp_dir)\google-java-format-1.18.1-all-deps.jar"
+$gjf_release_url = "https://github.com/google/google-java-format/releases/download/v1.18.1/google-java-format-1.18.1-all-deps.jar"
 
 if (-Not (Test-Path $gjf_path)) {
     New-Item -ItemType Directory -Path $temp_dir
